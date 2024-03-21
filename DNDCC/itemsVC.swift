d4fe021d21a3c37067d10f009f5380da0bad6d4b
Groblin-Garbage <69373464+Groblin-Garbage@users.noreply.github.com>
2024-03-21 14:19:28 -0400

    VC's added
    added all the VC's and created classes (03/21/24)

---
+++ DNDCC/itemsVC.swift
@@ -0,0 +1,29 @@
+//
+//  itemsVC.swift
+//  DNDCC
+//
+//  Created by Genghis Khan on 3/21/24.
+//
+
+import UIKit
+
+class itemsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
