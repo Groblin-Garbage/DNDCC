a1c4397d0f20dcb9e86c6faa88c23f2ce7bdce8c
Groblin-Garbage <69373464+Groblin-Garbage@users.noreply.github.com>
2024-04-01 14:25:26 -0400

    Added VC's and buttons
    I added a few new VC's and some buttons on the title screen 2/1/24

---
+++ DNDCC/proficienciesVC.swift
@@ -0,0 +1,29 @@
+//
+//  proficienciesVC.swift
+//  DNDCC
+//
+//  Created by Genghis Khan on 4/1/24.
+//
+
+import UIKit
+
+class proficienciesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DNDCC/raceDetailsVC.swift
@@ -0,0 +1,29 @@
+//
+//  raceDetailsVC.swift
+//  DNDCC
+//
+//  Created by Genghis Khan on 4/1/24.
+//
+
+import UIKit
+
+class raceDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
