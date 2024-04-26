e4fa4d3b9c41e5840d4449f6c6d944be9bf21284
Groblin-Garbage <69373464+Groblin-Garbage@users.noreply.github.com>
2024-04-26 14:34:26 -0400

    stat rolling
    added the code for the rolling of stats. Started code for selecting which stat to modify.

--- DNDCC/Character/StatVC.swift
+++ DNDCC/Character/StatVC.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class StatVC: UIViewController {
     
+    //str,dex,con,int,wis,cha
+    var stats: [Int] = [6,6,6,6,6,6]
+    var statsUsed: [Bool] = [false, false, false, false, false, false]
+    
     @IBOutlet weak var rollAllButton: UIButton!
     @IBOutlet weak var rollButton1: UIButton!
     @IBOutlet weak var rollButton2: UIButton!
@@ -17,7 +21,33 @@ class StatVC: UIViewController {
     @IBOutlet weak var rollButton5: UIButton!
     @IBOutlet weak var rollButton6: UIButton!
     
-
+    @IBOutlet weak var stat1: UIButton!
+    @IBOutlet weak var stat2: UIButton!
+    @IBOutlet weak var stat3: UIButton!
+    @IBOutlet weak var stat4: UIButton!
+    @IBOutlet weak var stat5: UIButton!
+    @IBOutlet weak var stat6: UIButton!
+    
+    @IBAction func stat1Button(_ sender: UIButton) {
+        print("test")
+        setStat(stat1)
+    }
+    @IBAction func stat2Button(_ sender: UIButton) {
+        print("test")
+    }
+    @IBAction func stat3Button(_ sender: UIButton) {
+        print("test")
+    }
+    @IBAction func stat4Button(_ sender: UIButton) {
+        print("test")
+    }
+    @IBAction func stat5Button(_ sender: UIButton) {
+        print("test")
+    }
+    @IBAction func stat6Button(_ sender: UIButton) {
+        print("test")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -35,38 +65,98 @@ class StatVC: UIViewController {
     }
     */
     @IBAction func button1(_ sender: UIButton) {
-        rollButton1.setImage("\(roll())D20", for: self)
+        rollButton1.setImage(UIImage(named: "\(roll())D20"), for: .normal)
     }
     @IBAction func button2(_ sender: UIButton) {
-        
+        rollButton2.setImage(UIImage(named: "\(roll())D20"), for: .normal)
     }
     @IBAction func button3(_ sender: UIButton) {
-        
+        rollButton3.setImage(UIImage(named: "\(roll())D20"), for: .normal)
     }
     @IBAction func button4(_ sender: UIButton) {
-        
+        rollButton4.setImage(UIImage(named: "\(roll())D20"), for: .normal)
     }
     @IBAction func button5(_ sender: UIButton) {
-        
+        rollButton5.setImage(UIImage(named: "\(roll())D20"), for: .normal)
     }
     @IBAction func button6(_ sender: UIButton) {
-        
+        rollButton6.setImage(UIImage(named: "\(roll())D20"), for: .normal)
     }
     @IBAction func rollAll(_ sender: UIButton) {
-        
+        rollButton1.setImage(UIImage(named: "\(roll())D20"), for: .normal)
+        rollButton2.setImage(UIImage(named: "\(roll())D20"), for: .normal)
+        rollButton3.setImage(UIImage(named: "\(roll())D20"), for: .normal)
+        rollButton4.setImage(UIImage(named: "\(roll())D20"), for: .normal)
+        rollButton5.setImage(UIImage(named: "\(roll())D20"), for: .normal)
+        rollButton6.setImage(UIImage(named: "\(roll())D20"), for: .normal)
     }
     
     func roll() -> Int {
-        var rolls = 4
+        var diceRolled = false
+        var rolls = 0
         var totalRoll = 0
-        for _ in 1...rolls {
+        var nums: [Int] = []
+        var goodNums: [Int] = []
+        while (!diceRolled) {
             var roll = Int.random(in: 1...6)
-            if (roll == 1) {
+            if (roll != 1) {
+                nums.append(roll)
                 rolls += 1
-            } else {
-                totalRoll += roll
+            }
+            if (rolls == 4) {
+                diceRolled = !diceRolled
             }
         }
+        nums.sort()
+        print(nums)
+        goodNums.append(contentsOf: nums.dropFirst(1))
+        print(goodNums)
+        totalRoll = goodNums[0] + goodNums[1] + goodNums[2]
         return totalRoll
+    }
+    
+    func setStat(statButton: UIButton) {
+        
+    }
+    
+    @objc func getAnswer() {
+        let ac = UIAlertController(title: "Pick which stat to assign the value rolled to", message: nil, preferredStyle: .alert)
+        let str = UIAlertAction(title: "Strength", style: .default) {
+            [unowned self, ac] (action: UIAlertAction) in
+            let answer = "Strength"
+            statsUsed[0] = !statsUsed[0]
+        }
+        ac.addAction(str)
+        let dex = UIAlertAction(title: "Dexterity", style: .default) {
+            [unowned self, ac] (action: UIAlertAction) in
+            let answer = "Dexterity"
+            statsUsed[1] = !statsUsed[1]
+        }
+        ac.addAction(dex)
+        let con = UIAlertAction(title: "Constitution", style: .default) {
+            [unowned self, ac] (action: UIAlertAction) in
+            let answer = "Constitution"
+            statsUsed[2] = !statsUsed[2]
+        }
+        ac.addAction(con)
+        let int = UIAlertAction(title: "Intelligence", style: .default) {
+            [unowned self, ac] (action: UIAlertAction) in
+            let answer = "Intelligence"
+            statsUsed[3] = !statsUsed[3]
+        }
+        ac.addAction(int)
+        let wis = UIAlertAction(title: "Wisdom", style: .default) {
+            [unowned self, ac] (action: UIAlertAction) in
+            let answer = "Wisdom"
+            statsUsed[4] = !statsUsed[4]
+        }
+        ac.addAction(wis)
+        let cha = UIAlertAction(title: "Charisma", style: .default) {
+            [unowned self, ac] (action: UIAlertAction) in
+            let answer = "Charisma"
+            statsUsed[5] = !statsUsed[5]
+        }
+        ac.addAction(cha)
+        present(ac, animated: true)
     }
 }
