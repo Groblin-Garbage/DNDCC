85fa2ad75c59774e358f353c8e8c4c4e849f754c
Groblin-Garbage <69373464+Groblin-Garbage@users.noreply.github.com>
2024-04-30 15:27:54 -0400

    subrace is not pog

--- DNDCC/Objects/SubclassObject.swift
+++ DNDCC/Objects/SubclassObject.swift
@@ -13,8 +13,19 @@ class SubclassObject: ClassObject {
     var subProfChoices: [ProficiencyObject]  // choices like athletics or animal handling
     var subProfAdded: [ProficiencyObject]  // guarunteed proficiencies like shields or martial weapons
     var subclassFeats: [String]
-    
+        
     init(subName: String, subProfAmount: Int, subProfChoices: [ProficiencyObject], subProfAdded: [ProficiencyObject], subclassFeats: [String]) {
+        super.init(name: <#T##String#>, hitDie: <#T##Int#>, profAmount: <#T##Int#>, profChoices: <#T##[ProficiencyObject]#>, profAdded: <#T##[ProficiencyObject]#>, savingThrows: <#T##[String]#>, startingEquipment: <#T##String#>, subclasses: <#T##[SubclassObject]#>, subclassLevel: <#T##Int#>, classFeats: <#T##[String]#>)
+        self.name = name
+        self.hitDie = hitDie
+        self.profAmount = profAmount
+        self.profChoices = profChoices
+        self.profAdded = profAdded
+        self.savingThrows = savingThrows
+        self.startingEquipment = startingEquipment
+        self.subclasses = subclasses
+        self.subclassLevel = subclassLevel
+        self.classFeats = classFeats
         self.subName = subName
         self.subProfAmount = subProfAmount
         self.subProfChoices = subProfChoices
